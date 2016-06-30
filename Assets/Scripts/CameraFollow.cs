383288ba201c255e8fe40aa447404890f1feafd7
Patryk Urban <p.urban@kainos.com>
2016-06-30 12:24:54 +0200

    Initial Unity project

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject TargetToFollow;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var newPosition = Vector3.Slerp(transform.position, TargetToFollow.transform.position, 2.0f);
+	    newPosition.y = transform.position.y;
+	    newPosition.z -= 4.5f;
+
+	    transform.position = newPosition;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Player _player;
+
+	void Start ()
+	{
+	    _player = GetComponent<Player>();
+	    if (_player == null)
+	    {
+	        Debug.LogError("Player not found");
+	    }
+	}
+	
+	void Update ()
+	{
+	    
+	}
+
+    void FixedUpdate()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        var velocity = 150*GetMovementDirection()*Time.deltaTime;
+        //transform.position += velocity;
+
+        _player.GetComponentInChildren<Rigidbody>().AddForce(velocity);
+    }
+
+    private Vector3 GetMovementDirection()
+    {
+        var direction = Vector3.zero;
+        //direction.y *= -1;  // invert the y-axis
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            direction.x += 1;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            direction.z += 1;
+
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            direction.z -= 1;
+        }
+
+        // Clamp the length of the vector to a maximum of 1.
+        if (direction.sqrMagnitude > 1)
+        {
+            direction.Normalize();
+        }
+
+        return direction;
+    }
+}
